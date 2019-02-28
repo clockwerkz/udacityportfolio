18de4a0d95dfa8520d22a46b6c486109026bd096
clockwerkz <clockwerkz@gmail.com>
2019-02-28 15:14:40 -0800

    first pass of project showcase in progress

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 import '../styles/main.scss';
 
-
-console.log("Hello");
+//Responsive Portfolio Project
+const projects = [
+    {
+        title: 'From Handlebars',
+        imgLocation: 'images/project01.JPG',
+        projectSynopsis: 'The first project assignment was to create a responsive portfolio site. I chose to highlight some of the projects I had worked on prior to the NanoDegree.',
+        liveLink: '#',
+        githubLink: '#'
+    }
+];

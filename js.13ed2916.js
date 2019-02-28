f6154d3d1c141454d30413e3d2ec6337f4f004c4
clockwerkz <clockwerkz@gmail.com>
2019-02-28 15:18:12 -0800

    Updates

---
+++ js.13ed2916.js
@@ -0,0 +1,8 @@
+parcelRequire=function(e,r,n,t){var i="function"==typeof parcelRequire&&parcelRequire,o="function"==typeof require&&require;function u(n,t){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!t&&f)return f(n,!0);if(i)return i(n,!0);if(o&&"string"==typeof n)return o(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[n][1][r]||r},p.cache={};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,p,l,l.exports,this)}return r[n].exports;function p(e){return u(p.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=i,u.register=function(r,n){e[r]=[function(e,r){r.exports=n},{}]};for(var f=0;f<n.length;f++)u(n[f]);if(n.length){var c=u(n[n.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=c:"function"==typeof define&&define.amd?define(function(){return c}):t&&(this[t]=c)}return u}({"tn6d":[function(require,module,exports) {
+
+},{}],"L/DU":[function(require,module,exports) {
+
+},{"./utils/normalize.css":"tn6d","./utils/reset.css":"tn6d","./..\\images\\markus-spiske-207946-unsplash.jpg":[["markus-spiske-207946-unsplash.bdbc3e52.jpg","v+Q7"],"v+Q7"]}],"QvaY":[function(require,module,exports) {
+"use strict";require("../styles/main.scss");var e=[{title:"From Handlebars",imgLocation:"images/project01.JPG",projectSynopsis:"The first project assignment was to create a responsive portfolio site. I chose to highlight some of the projects I had worked on prior to the NanoDegree.",liveLink:"#",githubLink:"#"}];
+},{"../styles/main.scss":"L/DU"}]},{},["QvaY"], null)
+//# sourceMappingURL=js.13ed2916.map
